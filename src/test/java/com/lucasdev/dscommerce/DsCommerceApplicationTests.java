800fa3bf4195bc827d4e59b0af0931e292dba9ce
lukasilverio <lukasilverio94@gmail.com>
2025-05-26 21:35:05 +0200

    create project

---
+++ src/test/java/com/lucasdev/dscommerce/DsCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdev.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
